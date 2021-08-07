b5281af7bf89567cc0a77940c3a8116dc8c76914
Sukhendra Rajawat <523sukh@gmail.com>
2021-08-07 17:06:01 +0530

    solving CORS ERROR

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -20,6 +20,17 @@ const {
 
 const router = express.Router();
 
+router.options("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Authorization, Content-Length, X-Requested-With, Accept"
+  );
+  res.sendStatus(200);
+});
+
 router.post("/sendOTP", sendOTP);
 router.post("/signin", verifyOTP, signin);
 router.post("/registerUser", isMobileAuthenticated, registerUser);
